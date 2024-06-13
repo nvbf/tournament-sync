aca1e0e95c4f74e9334e8c3d9939a3c745a416b3
Øystein W. Grændsen <oystein.w.graendsen@remarkable.no>
2024-06-13 19:57:10 +0200

    add OPTIONS

--- main.go
+++ main.go
@@ -91,7 +91,7 @@ func main() {
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if c.Request.Method == "OPTIONS" {
