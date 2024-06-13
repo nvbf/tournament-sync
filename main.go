6326f2e854fa9d776e2a24508b0f9cba3e1b6b68
Øystein W. Grændsen <oystein.w.graendsen@remarkable.no>
2024-06-13 17:54:55 +0200

    fix cors

--- main.go
+++ main.go
@@ -66,6 +66,7 @@ func main() {
 	matchesRouter.Use(auth.AuthMiddleware(firebaseApp)) // Apply the middleware here
 
 	syncRouter := router.Group("/sync/v1")
+	syncRouter.Use(CORSMiddleware())
 
 	admin.NewHTTPHandler(admin.HTTPOptions{
 		Service: adminService,
@@ -83,4 +84,20 @@ func main() {
 	})
 
 	log.Fatal(router.Run(":" + port))
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
