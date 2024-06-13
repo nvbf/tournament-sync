b91a5c48bf3c7413b3fa209013f858039ad9ced2
Øystein W. Grændsen <oystein.w.graendsen@remarkable.no>
2024-06-13 20:37:00 +0200

    fix cors

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
